2b3ebacbc1fbcb408061d4245a378405e185634b
muhbase <67451498+takanesachio@users.noreply.github.com>
2025-10-30 21:37:14 +0700

    update video

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -6,7 +6,6 @@ use App\Models\Category;
 use App\Models\Channel;
 use App\Models\Video;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -199,7 +198,7 @@ class VideoController extends Controller
             'description' => $validated['description'] ?? null,
             'status' => $validated['status'],
             'uploaded_at' => now(),
-            'created_by' => Auth::id(),
+            'created_by' => 1,
             'report_link' => $validated['report_link'] ?? null,
         ]);
 
